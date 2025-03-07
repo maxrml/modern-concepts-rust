a08a5aff2b39c2c07ad199a1d7cb15ff15aeb9e5
Erik <erik.breuer03@gmail.com>
2025-03-07 08:36:06 +0100

    added working stack and queue

---
+++ simple-datastructures/target/debug/deps/simple_datastructures-9d27c5d0db535da5.d
@@ -0,0 +1,8 @@
+/Users/I752559/Documents/Programmierkonzepte/rust-project/modern-concepts-rust/simple-datastructures/target/debug/deps/simple_datastructures-9d27c5d0db535da5: src/main.rs src/stack.rs src/queue.rs src/list.rs
+
+/Users/I752559/Documents/Programmierkonzepte/rust-project/modern-concepts-rust/simple-datastructures/target/debug/deps/simple_datastructures-9d27c5d0db535da5.d: src/main.rs src/stack.rs src/queue.rs src/list.rs
+
+src/main.rs:
+src/stack.rs:
+src/queue.rs:
+src/list.rs:
