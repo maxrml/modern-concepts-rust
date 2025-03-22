5661ccd9b314719c1ba1bd39974d988632d9e7fc
Thi Quynh Nhi Duong <duongtiq@schaeffler.com>
2025-03-22 21:31:19 +0100

    concurrency_forkjoin

--- concurrency_forkjoin/src/list.rs
+++ concurrency_forkjoin/src/list.rs
@@ -17,17 +17,15 @@ where
 
     let mid = data.len() / 2;
     let (left, right) = data.split_at(mid);
-    //println!("Forking: Left = {:?}, Right = {:?}", left, right);
 
     let (left_result, right_result) = forkjoin(
         || parallel_map(left, f),
         || parallel_map(right, f),
     );
 
-    //println!("Joining: Left Result = {:?}, Right Result = {:?}", left_result, right_result);
-
     [left_result, right_result].concat()
 }
+
 // Parallel Reduce using ForkJoin
 pub fn parallel_reduce<T, F>(data: &[T], f: &F) -> T
 where
@@ -44,13 +42,12 @@ where
 
     let mid = data.len() / 2;
     let (left, right) = data.split_at(mid);
-    //println!("Forking for reduce: Left = {:?}, Right= {:?}", left, right);
+
 
     let (left_result, right_result) = forkjoin(
         || parallel_reduce(left, f),
         || parallel_reduce(right, f),
     );
-
-    //println!("Joining results: {:?} and {:?}", left_result, right_result);
+   
     f(&left_result, &right_result)
 }
