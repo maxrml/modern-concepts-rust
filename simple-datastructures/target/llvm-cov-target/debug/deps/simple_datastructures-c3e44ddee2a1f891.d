6b420c333bb16dcb2b41049de493da7a7e320c08
MAX ROMMEL <max.rommel@sap.com>
2025-05-11 21:49:15 +0200

    Added readme information for the tests

---
+++ simple-datastructures/target/llvm-cov-target/debug/deps/simple_datastructures-c3e44ddee2a1f891.d
@@ -0,0 +1,9 @@
+/Users/I751109/Documents/RustProjects/modern-concepts-rust/simple-datastructures/target/llvm-cov-target/debug/deps/simple_datastructures-c3e44ddee2a1f891: src/main.rs src/stack.rs src/queue.rs src/list.rs src/datastructure.rs
+
+/Users/I751109/Documents/RustProjects/modern-concepts-rust/simple-datastructures/target/llvm-cov-target/debug/deps/simple_datastructures-c3e44ddee2a1f891.d: src/main.rs src/stack.rs src/queue.rs src/list.rs src/datastructure.rs
+
+src/main.rs:
+src/stack.rs:
+src/queue.rs:
+src/list.rs:
+src/datastructure.rs:
