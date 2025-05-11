b340181b6a5e2f629f3bc7684793a43d60046511
I750422 <michelle.ingerl@sap.com>
2025-05-11 21:18:23 +0200

    pdf added

--- threadpool-functional/target/debug/deps/threadpool-fa448cc8b79f8f10.d
+++ threadpool-functional/target/debug/deps/threadpool-fa448cc8b79f8f10.d
@@ -1,5 +1,5 @@
-/Users/I750422/Documents/VS/Rust repo/modern-concepts-rust/threadpool-functional/target/debug/deps/libthreadpool-fa448cc8b79f8f10.rmeta: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/threadpool-1.8.1/src/lib.rs
+/Users/I750422/Documents/rust/modern-concepts-rust/threadpool-functional/target/debug/deps/libthreadpool-fa448cc8b79f8f10.rmeta: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/threadpool-1.8.1/src/lib.rs
 
-/Users/I750422/Documents/VS/Rust repo/modern-concepts-rust/threadpool-functional/target/debug/deps/threadpool-fa448cc8b79f8f10.d: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/threadpool-1.8.1/src/lib.rs
+/Users/I750422/Documents/rust/modern-concepts-rust/threadpool-functional/target/debug/deps/threadpool-fa448cc8b79f8f10.d: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/threadpool-1.8.1/src/lib.rs
 
 /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/threadpool-1.8.1/src/lib.rs:
--- threadpool-functional/target/debug/deps/workerpool-45f87bd6e937f66b.d
+++ threadpool-functional/target/debug/deps/workerpool-45f87bd6e937f66b.d
@@ -1,5 +1,5 @@
-/Users/I750422/Documents/VS/Rust repo/modern-concepts-rust/threadpool-functional/target/debug/deps/libworkerpool-45f87bd6e937f66b.rmeta: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/workerpool-1.2.1/src/lib.rs
+/Users/I750422/Documents/rust/modern-concepts-rust/threadpool-functional/target/debug/deps/libworkerpool-45f87bd6e937f66b.rmeta: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/workerpool-1.2.1/src/lib.rs
 
-/Users/I750422/Documents/VS/Rust repo/modern-concepts-rust/threadpool-functional/target/debug/deps/workerpool-45f87bd6e937f66b.d: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/workerpool-1.2.1/src/lib.rs
+/Users/I750422/Documents/rust/modern-concepts-rust/threadpool-functional/target/debug/deps/workerpool-45f87bd6e937f66b.d: /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/workerpool-1.2.1/src/lib.rs
 
 /Users/I750422/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/workerpool-1.2.1/src/lib.rs:
